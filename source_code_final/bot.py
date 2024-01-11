31538356d5c2bc2c26e8c73e329584fc332c7c1b
CMC-Deploy <CMC-Deploy@iurdmoacsp.onmicrosoft.com>
2024-01-11 11:07:31 +0700

    update cors

--- source_code_final/bot.py
+++ source_code_final/bot.py
@@ -1,10 +1,14 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from cleaner import clean_corpus
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, jsonify, make_response
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
-CORPUS_FILE = "chat.txt"
+# CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app)
+
+CORPUS_FILE = ".\source_code_final\\chat.txt"
 
 chatbot = ChatBot("Chatpot")
 
@@ -12,8 +16,10 @@ trainer = ListTrainer(chatbot)
 cleaned_corpus = clean_corpus(CORPUS_FILE)
 trainer.train(cleaned_corpus)
 
+# @app.route("/")
 
 @app.route('/message', methods=['POST'])
+@cross_origin("*")
 def post_message():
     message = request.form.get('message')
     print("message", message)
